b70956f19bdc4f58f585b7e110dff329cbe4ca12
Santyrc032505 <santirodriguez0325@gmail.com>
2023-09-26 14:41:20 -0500

    Initial commit

---
+++ app/src/main/java/com/edu/ue/pajartar/InicioDeSesion.java
@@ -0,0 +1,14 @@
+package com.edu.ue.pajartar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioDeSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
